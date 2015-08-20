00f15cb71ef1641c2e125abca008ee7fab1d64cd
Patrick Perini <pcperini@gmail.com>
2015-08-19 19:34:23 -0700

    launch image

---
+++ BlueSkies/LaunchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchViewController.swift
+//  BlueSkies
+//
+//  Created by PATRICK PERINI on 8/19/15.
+//  Copyright (c) 2015 AppCookbook. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+}
